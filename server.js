b3a5547ef9c31f5f0aad98853e44e80556d340f1
shenry45 <shawn45henry@gmail.com>
2019-08-07 18:46:32 -0700

    Post / account add

--- server.js
+++ server.js
@@ -43,9 +43,27 @@ server.get('/:value', async (req, res) => {
   }
 });
 
-server.post('/:id', (req, res) => {
-  try {
+server.post('/', async (req, res) => {
+  const account = req.body;
 
+  try {
+    if (account.name && account.budget) {
+      const accountInfo = await db('accounts').insert(account);
+  
+      if (accountInfo) {
+        res.status(200).json({
+          accountInfo
+        })
+      } else {
+        res.status(500).json({
+          message: "Internal Server Error"
+        })
+      }
+    } else {
+      res.status(400).json({
+        message: "Required fields not found"
+      })
+    }  
   } catch (err) {
     res.status(500).json({
       message: err.message
